6dc2dbdfb3878fd3a61c2be43b9f1718c651bd24
Frank Chan <fuhranku@users.noreply.github.com>
2015-05-28 03:10:31 -0400

    Added functionality and formatting

---
+++ scrolling.js
@@ -0,0 +1,13 @@
+$(function() {
+		  $('a[href*=#]:not([href=#])').click(function() {
+		    
+		      var target = $(this.hash);
+		      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		      if (target.length) {
+		        $('html,body').animate({
+		          scrollTop: target.offset().top
+		        }, 1000);
+		        return false;
+		      }
+		  });
+		}); 
