4df66cee05d2a1d2d678e57c4bdb995a1d7485e0
Frank Chan <fc249@cornell.edu>
2016-09-01 23:22:15 -0400

    remove unimplemented navigation routes

--- services/navigationConfigSvc.js
+++ services/navigationConfigSvc.js
@@ -1,27 +1,8 @@
 var navigationOptions = [{
   category : "MYSELF",
-  path : "myself",
+  path : "myself/school",
   subsections : [
     { label : "School", path : "myself/school"},
-    { label : "Work", path : "myself/work"},
     { label : "Projects", path : "myself/projects"}
-  ]
-},
-{
-  category : "WIB&WIT",
-  path: "wibwit",
-  subsections : [
-    { label : "All", path : "wibwit"},
-    { label : "Food", path : "wibwit/food"},
-    { label : "Places", path : "wibwit/places"}
-  ]
-},
-{
-  category : "BLOG",
-  path: "blog",
-  subsections : [
-    { label : "All", path : "blog"},
-    { label : "Code", path : "blog/code"},
-    { label : "Life", path : "blog/life"}
   ]
 }];
